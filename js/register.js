e9cc5bde3e76cb254269bc9d5777b339d16d8cc9
Nauu' s <nahuelalonsouy@gmail.com>
2023-08-14 20:01:24 -0300

    change directories and validation password register

--- js/register.js
+++ js/register.js
@@ -6,6 +6,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const databaseKey = 'userDatabase';
     const database = loadDatabase(databaseKey);
     const currentUser = localStorage.getItem('currentUser');
+
+    const messageElement = document.getElementById("message");
     console.log(database);
 
     if (currentUser) {
@@ -14,22 +16,33 @@ document.addEventListener('DOMContentLoaded', () => {
 
     registerForm.addEventListener('submit', (e) => {
         e.preventDefault();
+        messageElement.textContent = "";
         const username = document.getElementById('registerUsername').value;
         const email = document.getElementById('registerEmail').value;
         const password = document.getElementById('registerPassword').value;
+        const confirmPasswordInput = document.getElementById("verifyPassword").value;
+        const existingUser = database.users.find(user => user.email === email);
 
-        // Verificar si el usuario ya existe
-        const existingUser = database.users.find(user => user.username === username || user.email === email);
-        if (existingUser) {
-            messageDiv.innerHTML = 'El usuario ya existe.';
+
+        if (password !== confirmPasswordInput) {
+            messageElement.textContent = "Las contraseñas no coinciden.";
+            console.log("las contraseñas no coincidieron");
             return;
+
+        } else if (existingUser) {
+            // Verificar si el usuario ya existe
+            messageDiv.innerHTML = 'El correo ya se encuentra registrado.';
+            return;
+        } else {
+            // Crear nuevo usuario
+            const newUser = { username, email, password };
+            database.users.push(newUser);
+            saveDatabase(databaseKey, database);
+            messageDiv.innerHTML = 'Usuario registrado con éxito.';
         }
 
-        // Crear nuevo usuario
-        const newUser = { username, email, password };
-        database.users.push(newUser);
-        saveDatabase(databaseKey, database);
-        messageDiv.innerHTML = 'Usuario registrado con éxito.';
+
+
     });
 });
 
