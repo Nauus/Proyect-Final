1e77f17c6e07ae078e4f96e1ae826297b498b0d9
Nauu' s <nahuelalonsouy@gmail.com>
2023-08-19 20:29:54 -0300

    update all files

--- js/localStorageUtils.js
+++ js/localStorageUtils.js
@@ -1,10 +1,10 @@
-export function loadDatabase (databaseKey) {
-    const database = JSON.parse(localStorage.getItem(databaseKey)) || { users: [] };
+export function loadDatabase (databaseKey) { // hacemos una funcion que mas tarde vamos a importar , para poder cargar la database en otros documentos js
+    const database = JSON.parse(localStorage.getItem(databaseKey)) || { users: [] }; // simulamos la base de datos utilizando localstorage
     return database;
 }
 
-export function saveDatabase (databaseKey, database) {
-    localStorage.setItem(databaseKey, JSON.stringify(database));
+export function saveDatabase (databaseKey, database) { // hacemos una function que va a ser util para poder guardar usuarios dentro de la misma
+    localStorage.setItem(databaseKey, JSON.stringify(database)); // para poder guardar usuarios dentro de nuestro localstorage tenemos que
 }
 
 export function clearCurrentUser () {
