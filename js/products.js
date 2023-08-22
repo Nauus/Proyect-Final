1778832c00177a6ddc95b63c8902050d00e9f7f5
jose perazza <joseperazza100@gmail.com>
2023-08-22 10:41:06 -0300

    asdjkfhdujfdksguasdhlfjkqd

--- js/products.js
+++ js/products.js
@@ -1,26 +1,21 @@
 const currentUser = localStorage.getItem('currentUser');
 
-
-
 if (!currentUser) {
     window.location.href = 'login.html';
 }
 
-
-
 document.addEventListener('DOMContentLoaded', () => {
     const productListContainer = document.getElementById('car-list-container');
+    const selectedCategoryID = localStorage.getItem('catID');
+    const productsUrl = `https://japceibal.github.io/emercado-api/cats_products/${selectedCategoryID}.json`;
 
-    // URL de la colección de productos en formato JSON
-    const productsUrl = 'https://japceibal.github.io/emercado-api/cats_products/101.json';
-
-    // Realizar la petición GET a la URL
+   
     fetch(productsUrl)
         .then(response => response.json())
         .then(data => {
             const products = data.products;
 
-            // Generar elementos HTML para cada producto
+            
             products.forEach(product => {
                 const productCard = document.createElement('div');
                 productCard.classList.add('card', 'mb-3');
