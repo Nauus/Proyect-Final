45dcd85a661125f73ada500835748a396f14590d
Nauu' s <nahuelalonsouy@gmail.com>
2023-10-14 19:40:30 -0300

    responsive product-info

--- js/index.js
+++ js/index.js
@@ -1,7 +1,4 @@
 
-
-
-
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("autos").addEventListener("click", function () {
         localStorage.setItem("catID", 101);
