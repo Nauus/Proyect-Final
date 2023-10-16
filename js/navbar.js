ab5bf6626c56c918992fad9d92489d8a8c72c106
Nauu' s <nahuelalonsouy@gmail.com>
2023-10-15 21:45:15 -0300

    changes

--- js/navbar.js
+++ js/navbar.js
@@ -3,12 +3,15 @@ fetch('navbar.html')
     .then(data => {
         document.getElementById('navbarContainer').innerHTML = data;
 
+        // Dispara el evento personalizado cuando el navbar se haya cargado
+        const navbarLoadedEvent = new Event('navbarLoaded');
+        document.dispatchEvent(navbarLoadedEvent);
     })
     .catch(error => {
         console.error('Error al cargar el navbar:', error);
     });
-
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('navbarLoaded', () => {
+    // Código de "my-profile.js" relacionado con el navbar
     const navbarProfilePicture = document.getElementById('navbarProfilePicture');
     const storedProfilePicture = localStorage.getItem('profilePicture_' + localStorage.getItem('currentUser'));
 
