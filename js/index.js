c0110e0e08516b6caadf7a06c94fb33079c58e70
Jose <138242710+perazzajose@users.noreply.github.com>
2023-10-20 17:07:20 -0300

    Squashed commit of the following:
    commit 3e6b88d97e0a768263d50988d0ed231243e4ce34
    Author: Camila <altezmendezcamila@gmail.com>
    Date:   Wed Oct 18 20:52:55 2023 -0300
    
        merge camila
    
    commit d4ee8bd3a7cdfeae35e1451045dfecc2276b8425
    Author: Nauu' s <nahuelalonsouy@gmail.com>
    Date:   Tue Oct 17 17:18:26 2023 -0300
    
        cart update
    
    commit 7d174934e5616ac3af269cf15aaa74a750379bb3
    Author: Nauu' s <nahuelalonsouy@gmail.com>
    Date:   Tue Oct 17 16:19:10 2023 -0300
    
        cart actualizado
    
    commit 93493d64bcc1ffbbd913fc8f1797d4452f9e674e
    Author: Nauu' s <nahuelalonsouy@gmail.com>
    Date:   Mon Oct 16 06:15:01 2023 -0300
    
        entrega 6
    
    commit c6ac8d7cbb3a627b04155a17ff9a8c420023db91
    Author: Nauu' s <nahuelalonsouy@gmail.com>
    Date:   Mon Oct 16 04:26:41 2023 -0300
    
        fix and bugs
    
    commit 757a1fc9698aeb163b31885d308def0db924df5f
    Merge: ab5bf66 95612f2
    Author: Nauu' s <nahuelalonsouy@gmail.com>
    Date:   Sun Oct 15 21:45:24 2023 -0300
    
        Merge branch 'main' of https://github.com/Nauus/Proyect-Final
    
    commit ab5bf6626c56c918992fad9d92489d8a8c72c106
    Author: Nauu' s <nahuelalonsouy@gmail.com>
    Date:   Sun Oct 15 21:45:15 2023 -0300
    
        changes
    
    commit 95612f2cbcbfe32f39a4a7e7c4cddcdde7037a09
    Author: Camila <altezmendezcamila@gmail.com>
    Date:   Sun Oct 15 21:40:28 2023 -0300
    
        preguntas frecuentes
    
    commit 534df9d0827dc81ceadd9ff2751ad20a18cc4160
    Merge: 897d83f db94773
    Author: Camila <altezmendezcamila@gmail.com>
    Date:   Sun Oct 15 20:52:53 2023 -0300
    
        Merge branch 'main' of https://github.com/Nauus/Proyect-Final into camila
    
    commit 897d83ffc12a34bc06467ead3603574626601d08
    Author: Camila <altezmendezcamila@gmail.com>
    Date:   Sun Oct 15 20:52:28 2023 -0300
    
        cambios rama camila
    
    commit db94773dd2a42bd81c14c9d2974041c17934f36f
    Author: Nauu' s <nahuelalonsouy@gmail.com>
    Date:   Sun Oct 15 20:51:49 2023 -0300
    
        modified index
    
    commit c096530c46f69fe385f554bc719d25ea43874cbb
    Merge: 79ad1e5 3e178c0
    Author: Nauu' s <nahuelalonsouy@gmail.com>
    Date:   Sun Oct 15 19:06:16 2023 -0300
    
        Merge branch 'camila'
    
    commit 3e178c04cd31a9e2dcbbcf6c0b509060f30d54ac
    Author: Camila <altezmendezcamila@gmail.com>
    Date:   Sun Oct 15 19:05:35 2023 -0300
    
        cambis index
    
    commit 79ad1e5f555c2975db120cbcd25cb8c6c26c2838
    Author: Nauu' s <nahuelalonsouy@gmail.com>
    Date:   Sun Oct 15 18:57:59 2023 -0300
    
        entrega 5
    
    commit 66c3e3f76cccb0eed7871baa548a77525d34e5d7
    Author: Camila <altezmendezcamila@gmail.com>
    Date:   Sun Oct 15 18:31:08 2023 -0300
    
        merge camila
    
    commit 45dcd85a661125f73ada500835748a396f14590d
    Author: Nauu' s <nahuelalonsouy@gmail.com>
    Date:   Sat Oct 14 19:40:30 2023 -0300
    
        responsive product-info
    
    commit 34a871e32fa91658aba32b2c6a81c974a1bf8473
    Author: Nauu' s <nahuelalonsouy@gmail.com>
    Date:   Sat Oct 14 19:20:03 2023 -0300
    
        upgrade page
    
    commit a2f0ea95881ab4a9668a1a0b15e6f8841f56e61a
    Author: Camila <altezmendezcamila@gmail.com>
    Date:   Sat Oct 14 14:02:49 2023 -0300
    
        cambios camila
    
    commit c3e115aa6d9fa0b109c469dd012a959e239a725b
    Author: Nauu' s <nahuelalonsouy@gmail.com>
    Date:   Fri Oct 13 21:49:00 2023 -0300
    
        add discount, function, check the code
    
    commit 07b80b31df685a07e106aab1e93c65da62a0805d
    Merge: 3ffbd7b b52ebf0
    Author: Jose <138242710+perazzajose@users.noreply.github.com>
    Date:   Thu Oct 12 14:37:14 2023 -0300
    
        arreglo pdf jose
    
    commit 3ffbd7b23b75011393ad7c195285cd9f1b70ccc5
    Merge: e3d0fa7 5b9ac26
    Author: Nauu' s <nahuelalonsouy@gmail.com>
    Date:   Thu Oct 12 10:13:20 2023 -0300
    
        Merge branch 'xd'
    
    commit e3d0fa79ef021152f3d013f91cf6c1517b13e917
    Author: Nauu' s <nahuelalonsouy@gmail.com>
    Date:   Thu Oct 12 08:55:00 2023 -0300
    
        partes video

--- js/index.js
+++ js/index.js
@@ -18,3 +18,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
+const currentUser = localStorage.getItem('currentUser');
+
+if (!currentUser) {
+    window.location.href = 'login.html';
+}
