db7586c5ab5b52e63c8e7afa204ab4ca09c120cb
juaniramella <nachoosq@gmail.com>
2023-11-09 13:50:46 -0300

    Validaciones

--- js/my-profile.js
+++ js/my-profile.js
@@ -50,7 +50,7 @@ document.getElementById('btnShowOldPassword').addEventListener('click', function
       })
   })()
 
-// Cambios en localStorage y validaciones.
+// Cambiar value de inputs a los datos del localStorage.
 
 document.addEventListener("DOMContentLoaded", function() {
 
@@ -74,10 +74,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
     document.getElementById("name").value = currentUserInfo.username;
     document.getElementById("name2").value = currentUserInfo.secondname;
-    document.getElementById("lastaname").value = currentUserInfo.lastname;
+    document.getElementById("lastname").value = currentUserInfo.lastname;
     document.getElementById("lastname2").value = currentUserInfo.secondlastname;
     document.getElementById("email").value = currentUserInfo.email;
     document.getElementById("phoneNumber").value = currentUserInfo.phonenumber;
+  });
+
+// Validar que el value del input de contraseña actual sea correcto, y dar feedback al usuario.
 
   currentPasswordInput.addEventListener("input", function() {
 
@@ -102,6 +105,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
   }});
 
+// En caso de que el usuario quiera cambiar la contraseña, dar feedback de si el value del input es correcto.
+
   newPasswordInput.addEventListener("input", function() {
 
   const repeatErrorDiv = document.getElementById("repeatErrorDiv");
@@ -121,28 +126,108 @@ document.addEventListener("DOMContentLoaded", function() {
   };
 });
 
+// Aplicar cambios a los datos de la cuenta en localStorage.
+
     document.getElementById('applyChanges').addEventListener('click', function (event) {
         event.preventDefault();
-        if (String(currentPasswordInput.value) == String(currentUserInfo.password)) {
-        currentUserInfo.username = `${document.getElementById("name").value}`;
-        currentUserInfo.secondname = `${document.getElementById("name2").value}`;
-        currentUserInfo.lastname = `${document.getElementById("lastaname").value}`;
-        currentUserInfo.secondlastname = `${document.getElementById("lastname2").value}`;
-        currentUserInfo.email = `${document.getElementById("email").value}`;
 
-        if ((document.getElementById("phoneNumber").value.length >= 9) && (document.getElementById("phoneNumber").value.length <= 13)) {
-          currentUserInfo.phonenumber = `${document.getElementById("phoneNumber").value}`;
+        if (String(currentPasswordInput.value) == String(currentUserInfo.password)) {
+
+          function name_lastnameIsValidInput(inputElement) {
+            const regex = /^[A-Za-z]+$/;
+            
+            return inputElement.value.trim().length >= 2 && regex.test(inputElement.value.trim());
         }
 
-        if ((newPasswordInput.value.length >= 6)) {
-          currentUserInfo.password = `${document.getElementById("newPassword").value}`;
-        };
-
+        function isValidEmail(email) {
+          var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+          return emailRegex.test(email);
       };
 
-        localStorage.setItem('userDatabase', JSON.stringify(existingData));
-        });
+      function isValidPhoneNumber(inputElement) { 
+            
+        return (((inputElement.length >= 9) || (inputElement.length >= 15)));
+      };
+
+      let nameIsValid = true
+      let name2IsValid = true
+      let lastnameIsValid = true
+      let lastname2IsValid = true
+      let emailIsValid = true
+      let phoneNumberIsValid = true
+      let newPasswordIsValid = true
+
+          if (name_lastnameIsValidInput(document.getElementById("name"))) {
+            console.log("Nombre es válido");
+            currentUserInfo.username = `${document.getElementById("name").value}`;
+          } else {
+            nameIsValid = false;
+            console.log(nameIsValid);
+          };
+
+          if (name_lastnameIsValidInput(document.getElementById("name2"))) {
+            console.log("Segundo nombre es válido");
+            currentUserInfo.secondname = `${document.getElementById("name2").value}`;
+          } else {
+            name2IsValid = false;
+          };
+
+          if (name_lastnameIsValidInput(document.getElementById("lastname"))) {
+            console.log("Apellido es válido");
+            currentUserInfo.lastname = `${document.getElementById("lastname").value}`;
+          } else {
+            lastnameIsValid = false;
+          };
+
+          if (name_lastnameIsValidInput(document.getElementById("lastname2"))) {
+            console.log("Segundo apellido es válido");
+            currentUserInfo.secondlastname = `${document.getElementById("lastname2").value}`;
+          } else {
+            lastname2IsValid = false;
+          };
+          
+          if (isValidEmail(document.getElementById("email").value)) {
+            console.log("Email correcto.");
+            currentUserInfo.email = `${document.getElementById("email").value}`;
+          } else {
+            emailIsValid = false;
+          };
+    
+          if (isValidPhoneNumber(document.getElementById("phoneNumber").value)) {
+            console.log("Número de teléfono largo suficiente");
+            currentUserInfo.phonenumber = `${document.getElementById("phoneNumber").value}`;
+          } else {
+            phoneNumberIsValid = false;
+          };
+
+            if (newPasswordInput.value.length >= 1 && newPasswordInput.value.length <= 5) {
+              newPasswordIsValid = false;
+            } else if (newPasswordInput.value.length === 0 && newPasswordInput.value.length >= 6) {
+              currentUserInfo.password = `${document.getElementById("newPassword").value}`;
+            };
+
+          if((nameIsValid && name2IsValid && lastnameIsValid && lastname2IsValid && emailIsValid && phoneNumberIsValid && newPasswordIsValid === true)) {
+          Swal.fire({
+              icon: 'success',
+              title: 'Cambios realizados',
+              text: "Los datos de su cuenta han sido cambiados exitosamente.",
+            }).then((result) => {
+              if (result.isConfirmed) {
+                localStorage.setItem('userDatabase', JSON.stringify(existingData));
+                location.reload();
+
+              }
+            });
+          return;
+        } else {
+          console.log(((nameIsValid || name2IsValid || lastnameIsValid || lastname2IsValid || emailIsValid || phoneNumberIsValid || newPasswordIsValid === false)));
+
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: "Alguno de los campos presenta errores.",
+        })
+}
+
+      };
 });
-
-// Cambio de imagen de perfil
-
