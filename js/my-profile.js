562c804b8d016e34d022ad8929af0afc2e933bb2
juaniramella <nachoosq@gmail.com>
2023-11-09 02:16:08 -0300

    funcionalidad de myprofile.html

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,27 +1,86 @@
 import { loadDatabase, saveDatabase } from './localStorageUtils.js';
 
+document.getElementById('btnShowOldPassword').addEventListener('click', function() {
+    if(document.getElementById('currentPassword').getAttribute('type') == 'text') {
+        document.getElementById('currentPassword').setAttribute('type', 'password')
+    } else {
+    document.getElementById('currentPassword').setAttribute('type', 'text');
+    }
+    });
+
+    document.getElementById('btnShowNewPassword').addEventListener('click', function() {
+        if(document.getElementById('newPassword').getAttribute('type') == 'text') {
+            document.getElementById('newPassword').setAttribute('type', 'password')
+        } else {
+        document.getElementById('newPassword').setAttribute('type', 'text');
+        }
+        });
+
+(function () {
+    'use strict'
+  
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+
 document.addEventListener("DOMContentLoaded", function() {
     const storedData = localStorage.getItem('userDatabase');
 
-// Paso 2: Convierte el objeto en un objeto JavaScript
     const existingData = JSON.parse(storedData);
 
-// Paso 3: Agregar la propiedad "lastname" al objeto
-    existingData.users[0].secondname = '';
-    existingData.users[0].lastname = '';
-    existingData.users[0].secondlastname = '';
-    existingData.users[0].phonenumber = '';
+    const currentUserInfo = existingData.users[0];
 
-// Vuelve a guardar el objeto modificado en el localStorage
-    localStorage.setItem('userDatabase', JSON.stringify(existingData));
+    if (currentUserInfo) {
+        if (!currentUserInfo.secondname) {
+          currentUserInfo.secondname = '';
+        }
+      
+        if (!currentUserInfo.lastname) {
+          currentUserInfo.lastname = '';
+        }
+      
+        if (!currentUserInfo.secondlastname) {
+          currentUserInfo.secondlastname = '';
+        }
+      
+        if (!currentUserInfo.phonenumber) {
+          currentUserInfo.phonenumber = '';
+        }
+      }
 
-    document.getElementById("name").value = userObject.users[0].username;
-    document.getElementById("name2").value = userObject.users[0].secondname;
-    document.getElementById("lastaname").value = userObject.users[0].lastname;
-    document.getElementById("lastname2").value = userObject.users[0].secondlastname;
-    document.getElementById("email").value = userObject.users[0].email;
-    document.getElementById("phoneNumber").value = userObject.users[0].phonenumber;
-    document.getElementById("currentPassword").value = userObject.users[0].password;
+    document.getElementById("name").value = currentUserInfo.username;
+    document.getElementById("name2").value = currentUserInfo.secondname;
+    document.getElementById("lastaname").value = currentUserInfo.lastname;
+    document.getElementById("lastname2").value = currentUserInfo.secondlastname;
+    document.getElementById("email").value = currentUserInfo.email;
+    document.getElementById("phoneNumber").value = currentUserInfo.phonenumber;
+    document.getElementById("currentPassword").value = currentUserInfo.password;
 
+    document.getElementById('applyChanges').addEventListener('click', function (event) {
+        event.preventDefault();
 
-    });
+        currentUserInfo.username = `${document.getElementById("name").value}`;
+        currentUserInfo.secondname = `${document.getElementById("name2").value}`;
+        currentUserInfo.lastname = `${document.getElementById("lastaname").value}`;
+        currentUserInfo.secondlastname = `${document.getElementById("lastname2").value}`;
+        currentUserInfo.email = `${document.getElementById("email").value}`;
+        currentUserInfo.phonenumber = `${document.getElementById("phoneNumber").value}`;
+
+        if ((document.getElementById("newPassword").value.length >= 6)) {
+        currentUserInfo.password = `${document.getElementById("newPassword").value}`;
+        }
+
+        localStorage.setItem('userDatabase', JSON.stringify(existingData));
+        });
+});
